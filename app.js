5be34be146531d9db99cb833b334746f0bcc76d8
Mohammad Nahid Uddin Babor <mdsnbabor828@gmail.com>
2026-02-24 12:14:20 +0600

    Implement filter button toggle

--- app.js
+++ app.js
@@ -25,3 +25,42 @@ function counter() {
   rejectedCount.innerText = rejectedApplications.length;
 }
 counter();
+function toggleStyle(id) {
+  filterBtns.forEach((btn) => {
+    btn.classList.remove("btn-info");
+    btn.classList.add("btn-soft");
+  });
+
+  const selected = document.getElementById(id);
+  currentStatus = id;
+  selected.classList.remove("btn-soft");
+  selected.classList.add("btn-info");
+
+  if (id == "all-btn") {
+    totalApplication.classList.remove("hidden");
+    filterApplications.classList.add("hidden");
+    if (totalApplication.children.length === 0) {
+      noAvailableJobs.classList.remove("hidden");
+    } else {
+      noAvailableJobs.classList.add("hidden");
+    }
+  } else if (id == "Interview-btn") {
+    if (interviewApplications.length == 0) {
+      noAvailableJobs.classList.remove("hidden");
+    } else {
+      noAvailableJobs.classList.add("hidden");
+    }
+    totalApplication.classList.add("hidden");
+    filterApplications.classList.remove("hidden");
+    renderInterview();
+  } else if (id == "Rejected-btn") {
+    if (rejectedApplications.length == 0) {
+      noAvailableJobs.classList.remove("hidden");
+    } else {
+      noAvailableJobs.classList.add("hidden");
+    }
+    totalApplication.classList.add("hidden");
+    filterApplications.classList.remove("hidden");
+    renderRejected();
+  }
+}
